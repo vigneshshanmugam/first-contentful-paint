b949d9e430ec638af6c2f04c0eb1452ccf96185e
vigneshshanmugam <vignesh.shanmugam22@gmail.com>
2020-05-07 21:27:17 +0200

    setup microbundle for build

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # first-contentful-paint
-A JavaScript library for measuring First Contentful Paint (FCP) in the browser.
+
+A JavaScript library for measuring First Contentful Paint (FCP) in the browser
+that does not support the [Paint Timing API](https://w3c.github.io/paint-timing/).
--- src/first-contentful-paint.js
+++ src/first-contentful-paint.js
@@ -1,83 +1,80 @@
-(function(rAF) {
-  var firstContentfulPaint;
-  var isImage = false;
-  var currentNode;
-  var callbacks = [];
+const rAF = window.requestAnimationFrame;
+let callbacks = [];
+let firstContentfulPaint;
+let isImage = false;
+let currentNode;
 
-  function onFirstContentfulPaint(callback) {
-    callbacks.push(callback);
+function getFCP(callback) {
+  callbacks.push(callback);
+  reportFCP();
+}
+
+function reportFCP() {
+  if (firstContentfulPaint >= 0) {
+    callbacks.forEach(function(callback) {
+      callback(firstContentfulPaint, currentNode);
+    });
+    callbacks = [];
+  }
+}
+
+function isPainted(node) {
+  return node.offsetHeight > 0;
+}
+
+function isImageCompleted() {
+  if (currentNode.complete && isPainted(currentNode)) {
+    firstContentfulPaint = performance.now();
     reportFCP();
+  } else {
+    rAF(() => isImageCompleted());
   }
+}
 
-  function reportFCP() {
-    if (firstContentfulPaint >= 0) {
-      callbacks.forEach(function(callback) {
-        callback(firstContentfulPaint, currentNode);
-      });
-      callbacks = [];
-    }
-  }
-
-  function isVisible(node) {
-    return node.offsetHeight > 0;
-  }
-
-  function isImageCompleted() {
-    if (currentNode.complete && isVisible(currentNode)) {
-      firstContentfulPaint = performance.now();
-      reportFCP();
-    } else {
-      rAF(() => isImageCompleted());
-    }
-  }
-
-  function checkFCP() {
-    var iterator = document.createNodeIterator(
-      document.body,
-      NodeFilter.SHOW_ALL,
-      function(node) {
-        if (!node) {
-          return false;
-        }
-
-        var isNonEmptyTextNode =
-          node.nodeType === Node.TEXT_NODE &&
-          /[^s]/.test(node.nodeValue.trim());
-
-        if (isNonEmptyTextNode && isVisible(node.parentNode)) {
-          return isNonEmptyTextNode;
-        }
-
-        var isContentfulImage = node.tagName === "IMG" && node.src != "";
-
-        if (isContentfulImage) {
-          isImage = true;
-          return isContentfulImage;
-        }
-
+function checkFCP() {
+  const iterator = document.createNodeIterator(
+    document.body,
+    NodeFilter.SHOW_ALL,
+    function(node) {
+      if (!node) {
         return false;
-      },
-      false
-    );
-
-    currentNode = iterator.nextNode();
-
-    if (currentNode != null) {
-      if (isImage) {
-        rAF(isImageCompleted);
-      } else {
-        rAF(() => {
-          firstContentfulPaint = performance.now();
-          reportFCP();
-        });
       }
+
+      const isNonEmptyTextNode =
+        node.nodeType === Node.TEXT_NODE && /[^s]/.test(node.nodeValue.trim());
+
+      if (isNonEmptyTextNode && isPainted(node.parentNode)) {
+        return isNonEmptyTextNode;
+      }
+
+      const isContentfulImage = node.tagName === "IMG" && node.src != "";
+
+      if (isContentfulImage) {
+        isImage = true;
+        return isContentfulImage;
+      }
+
+      return false;
+    },
+    false
+  );
+
+  currentNode = iterator.nextNode();
+
+  if (currentNode != null) {
+    if (isImage) {
+      rAF(isImageCompleted);
     } else {
-      rAF(checkFCP);
+      rAF(() => {
+        firstContentfulPaint = performance.now();
+        reportFCP();
+      });
     }
+  } else {
+    rAF(checkFCP);
   }
+}
 
-  rAF(checkFCP);
+rAF(checkFCP);
 
-  self["perfMetrics"] = self["perfMetrics"] || {};
-  self["perfMetrics"]["onFirstContentfulPaint"] = onFirstContentfulPaint;
-})(requestAnimationFrame);
+export default getFCP;
